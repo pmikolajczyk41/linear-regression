95940e03f0c3e7d94f7c8681aa129773b0dc4ec9
Piotr Mikolajczyk <piomiko41@gmail.com>
2020-11-13 21:01:12 +0100

    Data; Loading data

---
+++ data/loading.py
@@ -0,0 +1,16 @@
+from collections import Collection
+from pathlib import Path
+from typing import Tuple
+
+
+def load(filepath: Path) -> Tuple[Collection, Collection]:
+    assert filepath.exists() and filepath.is_file()
+
+    with filepath.open() as file:
+        lines = file.readlines()
+
+    samples = map(lambda l: l.split(), lines)
+    samples = (tuple(map(lambda v: float(v), s)) for s in samples)
+    samples = ((s[:-1], s[-1]) for s in samples)
+
+    return tuple(zip(*samples))
